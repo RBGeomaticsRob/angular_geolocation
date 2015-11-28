5f3f840667309c2b27134d60bd7c2c2544fc9d83
RBGeomaticsRob <RBGeomaticsRob@users.noreply.github.com>
2015-11-28 23:52:21 +0000

    Locations Controller passing test to show list of locations

--- js/LocationsController.js
+++ js/LocationsController.js
@@ -1,3 +1,18 @@
 app.controller('LocationsController',[function() {
+  var self = this;
 
+  self.locations = {
+    "items": [
+      {
+        "location": "1",
+        "Lat": "51.23634",
+        "Long": "1.39560"
+      },
+      {
+        "location": "2",
+        "Lat": "51.24634",
+        "Long": "1.40560"
+      }
+    ]
+  };
 }]);
--- test/LocationsController.spec.js
+++ test/LocationsController.spec.js
@@ -10,4 +10,24 @@ describe('LocationsController', function() {
   it('initialises with an empty list of locations', function() {
     expect(ctrl.locations).toBeUndefined();
   });
+
+  describe('when looking for locations', function() {
+    var items = [
+      {
+        "location": "1",
+        "Lat": "51.23634",
+        "Long": "1.39560"
+      },
+      {
+        "location": "2",
+        "Lat": "51.24634",
+        "Long": "1.40560"
+      }
+    ];
+
+    it('displays locations', function() {
+      expect(ctrl.locations.items).toEqual(items);
+    });
+  });
 });
+
