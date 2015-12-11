957017c23aad00d6eb238d446de391f49ab0a718
RBGeomaticsRob <RBGeomaticsRob@users.noreply.github.com>
2015-12-11 17:20:24 +0000

    Stopped marker following centre

--- public/js/LocationsController.js
+++ public/js/LocationsController.js
@@ -12,34 +12,24 @@ app.controller('LocationsController',[function() {
   };
 
   self.setMap = function(setSite){
-    self.map = self.locations.items[setSite].map;
+    self.map = { center: { latitude: self.locations.items[setSite].latitude, longitude: self.locations.items[setSite].longitude }, zoom: 8};
   };
 
   self.siteCoords = function(setSite){
-    return self.locations.items[setSite].map.center;
+    return { latitude: self.locations.items[setSite].latitude, longitude: self.locations.items[setSite].longitude };
   };
 
   self.locations = {
     "items": [
       {
         "location": "0",
-        "map": {
-          center: {
-              latitude: 51.23634,
-              longitude: 1.39560
-            },
-          zoom: 8
-        }
+        latitude: 51.23634,
+        longitude: 1.39560
       },
       {
         "location": "1",
-        "map": {
-          center: {
-              latitude: 50.23634,
-              longitude: -1.39560
-            },
-          zoom: 8
-        }
+        latitude: 50.23634,
+        longitude: -1.39560
       }
     ]
   };
